3199782c1caa6908462265d3894a79bb39927802
Jamie Getty <jmg211@uclive.ac.nz>
2017-06-18 17:13:46 +1200

    Begin work on input processing. Will use a compiler to create your own commands.

---
+++ Compiler/compiler.cpp
@@ -0,0 +1,10 @@
+/*
+ * compiler.cpp
+ *
+ *  Created on: 14/06/2017
+ *      Author: jamie
+ */
+
+
+
+
---
+++ Compiler/compiler.h
@@ -0,0 +1,21 @@
+/*
+ * compiler.h
+ *
+ *  Created on: 14/06/2017
+ *      Author: jamie
+ *
+ *      Used to create user-defined commands
+ *      I am reading http://www.diku.dk/~torbenm/Basics/basics_lulu2.pdf
+ *
+ *      We need to perform lexical analysis to convert our commands into tokens, to be processed
+ *
+ *      We could also think of the processing of user commands as being interpreted
+ */
+
+#ifndef COMPILER_COMPILER_H_
+#define COMPILER_COMPILER_H_
+
+
+
+
+#endif /* COMPILER_COMPILER_H_ */
